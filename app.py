7c77671f4d322bf04f02e9d72ba1aeb31232b646
Parthavi5654 <parthavichovatiya308@gmail.com>
2024-06-02 19:28:32 +0530

    This is my first time to push a code inside git

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask , render_template , request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+
+
+
+@app.route('/weatherapp' , methods = ['POST' , "GET"])
+def get_weatherdate():
+    url = "http://api.weatherapi.com/v1/current.json"
+
+    parameters = {
+    'key' : request.form.get('appid')  , 
+    'q' : request.form.get("city")
+    }
+
+    row_data = requests.get(url ,params = parameters)   
+    Data = row_data.json()
+
+    return f"report : {Data}"
+
+
+
+if __name__ == '__main__':
+    app.run(host ="0.0.0.0")
